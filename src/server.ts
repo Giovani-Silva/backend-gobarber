088e095a2df0fb2161df8694dba2f93a971da675
Giovani Silva <P51901401@brasilprev.com.br>
2020-10-18 13:18:07 -0300

    prettier

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import express, { json, request, response } from "express";
+import express, { json, request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(json());
 
-app.get("/", (request, response) => {
-  response.json({ message: "Hello World" });
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello World' });
 });
 
 app.listen(3333, () => {
-  console.log("🚀 Back-end GoBarber started");
+  console.log('🚀 Back-end GoBarber started');
 });
