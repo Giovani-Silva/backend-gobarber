784b68824ecab287e1a10a6e0f92fb358b08b347
unknown <giovani.silva@live.com>
2020-12-06 20:01:43 -0300

    Primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,24 @@
-test(' sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import 'reflect-metadata';
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '12121212',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12121212');
+  });
+
+  it('should not be able to create two appointments on the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
