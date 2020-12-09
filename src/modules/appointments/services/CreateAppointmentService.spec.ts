e8ca6f01a6cd73b5fa53a872587adb6ed838b75d
unknown <giovani.silva@live.com>
2020-12-09 18:15:48 -0300

    test CreateUser

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import CreateAppointmentService from './CreateAppointmentService';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -18,7 +19,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('12121212');
   });
 
-  it('should not be able to create two appointments on the same time', () => {
-    expect(1 + 2).toBe(3);
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository2 = new FakeAppointmentRepository();
+    const createAppointment2 = new CreateAppointmentService(
+      fakeAppointmentsRepository2,
+    );
+
+    const appointmentDate = new Date();
+
+    await createAppointment2.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+    });
+
+    expect(
+      createAppointment2.execute({
+        date: appointmentDate,
+        provider_id: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
